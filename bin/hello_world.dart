74010f8afb521a54fa6cb3fab692b83683660ea4
Abdur Rahman <rahmanabdur15689@gmail.com>
2024-01-16 08:04:10 +0700

    hello world

---
+++ bin/hello_world.dart
@@ -0,0 +1,9 @@
+// untuk bahasa dart harus menggunakan function pembuka agar terbaca yaitu -> void main
+void main() {
+  print("Hello World");
+}
+// function print untuk menampilkan value yang dimasukkan
+
+// harus menggunakan titik koma (;) di akhir setiap baris seperti di java
+
+// untuk menjalankan program gunakan command => dart bin/hello_world.dart  "harus masuk dahulu ke folder bin di dart kemudian ke file yang ingin di jalankan"
---
+++ bin/komentar.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 10;
+  int b = 20;
+
+  print(a + b);
+  //print(a - b);
+
+  //ini adalah komentar yaitu tanda // yang mana itu tidak akan terdeteksi ketika kita mengeksekusi program
+}
