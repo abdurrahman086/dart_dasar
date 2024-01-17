d5c32f62ec535fb329eb425d9d9b5e06e699e16e
Abdur Rahman <rahmanabdur15689@gmail.com>
2024-01-18 01:54:43 +0700

    dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,12 @@
+// tipe data dynamic bisa menampung semua tipe data
+
+void main() {
+  dynamic variabel = 100;
+  print(variabel);
+
+  variabel = true;
+  print(variabel);
+
+  variabel = 'abdurrahman';
+  print(variabel);
+}
