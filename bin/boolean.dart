22f844ee91524935710e4c867f6c587ab2d153fc
Abdur Rahman <rahmanabdur15689@gmail.com>
2024-01-17 09:11:25 +0700

    boolean

---
+++ bin/boolean.dart
@@ -0,0 +1,16 @@
+main(List<String> args) {
+  checkRain();
+}
+
+checkRain() {
+  bool check;
+  int val1 = 12;
+  int val2 = 9;
+
+  // Assigning variable check
+  // value depending on condition
+  check = (val1 > val2);
+  print(check);
+}
+
+// boolean merupakan tipe data yang bernilai true atau false
