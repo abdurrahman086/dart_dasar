760b115f5914eaec1ac999ab74492fcfee7fcc7f
Abdur Rahman <rahmanabdur15689@gmail.com>
2024-01-17 08:34:44 +0700

    string

---
+++ bin/string.dart
@@ -0,0 +1,25 @@
+void main(){
+  String singleQuote = 'Hello World';
+  String doubleQuote = "Hello World";
+
+  // escape string
+  String hari = 'jum\'at';
+
+  // string interpolation
+  String name = 'Abdurrahman';
+  int age = 15;
+  String output = 'Hello $name, you are $age years old.';
+  
+
+
+  // output
+  print(singleQuote);
+  print(doubleQuote);
+  print(hari);
+  print(output);
+}
+
+//didalam string kita bisa menggunakan tanda " " atau ' '
+//kita bisa menggunakan escape string \ untuk menambahkan tanda ' ' ke string ketika kalimat memiliki petik
+// contoh escape string => (\n : Baris Baru), (\r : Kembali ke awal baris), (\t : Horizontal tab), (\v : Vertical tab), (\b : Hapus karakter sebelumnya), (\\ : \)
+//string interpolation => ${variabel} : merupakan cara memasukkan variabel kedalam string
