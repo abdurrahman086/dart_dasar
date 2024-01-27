31f3ba96f78a8ace07f302bdb389032bd9998cae
Abdur Rahman <rahmanabdur15689@gmail.com>
2024-01-27 17:26:48 +0700

    while loop

---
+++ bin/while_loop.dart
@@ -0,0 +1,12 @@
+// while loop adalah versi perulangan yang lebih sederhana dari for loop
+// di while loop hanya terdapat kondisi perulangan tanpa ada init statement dan post statement
+
+void main() {
+
+  var counter = 1;
+
+  while(counter <= 10) {
+    print('perulangan ke-$counter');
+    counter++; // untuk menjalankan 
+  }
+}
